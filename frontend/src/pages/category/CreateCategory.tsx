c890b36b5454f71cbfa6cff59537bd9616b8bc53
NyiWaingchit7 <nyiwaingchit5@gmail.com>
2024-09-19 22:12:36 +0630

    fix

--- frontend/src/pages/category/CreateCategory.tsx
+++ frontend/src/pages/category/CreateCategory.tsx
@@ -1,6 +1,6 @@
 import { HeadLine } from "../../component/HeadLine";
 import { Layout } from "../../component/layout/Layout";
-import { Form } from "./partials/Form";
+import { Form } from "./Partials/Form";
 
 export const CreateCategory = () => {
   return (
