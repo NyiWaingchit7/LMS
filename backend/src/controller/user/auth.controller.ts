21884832993b403fd4e09b8e5244aabb4aebac8d
NyiWaingchit7 <nyiwaingchit5@gmail.com>
2024-12-14 18:20:58 +0630

    auth-api

--- backend/src/controller/user/auth.controller.ts
+++ backend/src/controller/user/auth.controller.ts
@@ -5,7 +5,9 @@ import jwt from "jsonwebtoken";
 import { config } from "../../../utils/config";
 import NodeCache from "node-cache";
 import { getUserFromToken } from "../../../utils/auth";
+
 const cache = new NodeCache({ stdTTL: 660 });
+
 export const register = async (req: Request, res: Response) => {
   const { name, email, password, phone, assetUrl } = req.body;
   try {
@@ -42,7 +44,7 @@ export const login = async (req: Request, res: Response) => {
 
     const token = jwt.sign(student, config.jwtStudentSecret);
 
-    return res.status(200).json({ token });
+    return res.status(200).json({ token, student });
   } catch (error) {
     return res.status(500).json({ error });
   }
@@ -63,6 +65,7 @@ export const verify = async (req: Request, res: Response) => {
       });
 
       const token = jwt.sign(student, config.jwtStudentSecret);
+      cache.del(email);
       return res.status(200).json({ student, token });
     } else {
       return res.status(400).json({ message: "Verification fail." });
@@ -72,17 +75,98 @@ export const verify = async (req: Request, res: Response) => {
   }
 };
 
+export const myProfile = async (req: Request, res: Response) => {
+  try {
+    const user = getUserFromToken(req, res);
+    return res.status(200).json({ user });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+};
+
+export const profileDelete = async (req: Request, res: Response) => {
+  try {
+    const user = getUserFromToken(req, res) as any;
+    const id = Number(user?.id);
+    await prisma.student.delete({ where: { id } });
+    return res.status(200).json({ message: "Deleted Account Successfully." });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+};
+
+export const forgetPassword = async (req: Request, res: Response) => {
+  try {
+    //need validation
+    const { email } = req.body;
+    const isExist = await prisma.user.findFirst({ where: { email } });
+    if (!isExist)
+      return res
+        .status(404)
+        .json({ message: "There is no user with this Email." });
+    const code = generateRandomCode();
+    cache.set(email, code);
+
+    return res.status(200).json({ code });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+};
+export const forgetVerify = async (req: Request, res: Response) => {
+  try {
+    const { email, code } = req.body;
+    const cacheCode = cache.get(email);
+    if (!cacheCode) res.status(400).json({ message: "Your code is invalids" });
+    if (code === cacheCode) {
+      cache.del(email);
+      return res.status(200).json({ message: "Otp verify successfully." });
+    } else {
+      res.status(400).json({ message: "Your code is invalids" });
+    }
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+};
+
+export const forgetPasswordChange = async (req: Request, res: Response) => {
+  try {
+    const { new_password, confirm_password } = req.body;
+    const user = getUserFromToken(req, res) as any;
+
+    const data = await prisma.student.update({
+      where: { id: user.id },
+      data: { password: new_password },
+    });
+
+    return res.status(200).json({ message: "Password Changed Successfully." });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+};
+
+export const changePassword = async (req: Request, res: Response) => {
+  //need validation
+  try {
+    const { old_password, new_password, confirm_password } = req.body;
+    const user = getUserFromToken(req, res) as any;
+
+    const hash = user?.password as string;
+    const isOldPassword = await bcrypt.compare(old_password, hash);
+    if (!isOldPassword)
+      return res
+        .status(400)
+        .json({ message: "Your old password is not correct" });
+    const data = await prisma.student.update({
+      where: { id: user.id },
+      data: { password: new_password },
+    });
+    return res.status(200).json({ message: "Password Changed Successfully." });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+};
 export const generateRandomCode = (length = 6) => {
   const min = Math.pow(10, length - 1);
   const max = Math.pow(10, length) - 1;
   return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-export const myProfile = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const user = getUserFromToken(req, res);
-  return res.status(200).json({ user });
 };
--- backend/src/routes/user/auth.route.ts
+++ backend/src/routes/user/auth.route.ts
@@ -1,11 +1,28 @@
 import express from "express";
-import { login, register, verify } from "../../controller/user/auth.controller";
+import {
+  changePassword,
+  forgetPassword,
+  forgetPasswordChange,
+  forgetVerify,
+  login,
+  myProfile,
+  profileDelete,
+  register,
+  verify,
+} from "../../controller/user/auth.controller";
 import { show, update } from "../../controller/student.controller";
+import { usercheckauth } from "../../../utils/auth";
 export const userAuthRouter = express.Router();
 
 userAuthRouter.post("/register", register);
 userAuthRouter.post("/forget-password", register);
 userAuthRouter.post("/verify", verify);
 userAuthRouter.post("/log-in", login);
-userAuthRouter.get("/my-profile", show);
+userAuthRouter.get("/my-profile", myProfile);
+userAuthRouter.get("/delete-account", profileDelete);
+userAuthRouter.get("/forget-password", forgetPassword);
+userAuthRouter.get("/forget-password-change", forgetPasswordChange);
+userAuthRouter.get("/forget-verify", forgetVerify);
+userAuthRouter.get("/change-password", changePassword);
+
 userAuthRouter.post("/edit-profile", update);
