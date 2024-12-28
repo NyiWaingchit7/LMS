f5c86592be6f5d62a80a697141ad7d919b31677f
NyiWaingchit7 <nyiwaingchit5@gmail.com>
2024-12-28 15:34:08 +0630

    setting

---
+++ backend/src/validator/setting.validator.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from "express";
+import * as yup from "yup";
+
+export const schema = yup.object().shape({
+  email: yup.string().email(),
+});
+
+export const settingValidation = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    await schema.validate(req.body, { abortEarly: false });
+    next();
+  } catch (err: any) {
+    const errors: { [key: string]: string } = {};
+
+    if (err.inner) {
+      err.inner.map((error: any) => {
+        if (error.path) {
+          errors[error.path] = error.message;
+        }
+      });
+    }
+
+    return res.status(400).json({ errors });
+  }
+};
