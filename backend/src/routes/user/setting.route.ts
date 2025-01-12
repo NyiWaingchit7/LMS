2e08813185d22f9cbc8e3bc5c51871498efdbae0
NyiWaingchit7 <nyiwaingchit5@gmail.com>
2025-01-12 13:33:18 +0630

    setting

---
+++ backend/src/routes/user/setting.route.ts
@@ -0,0 +1,6 @@
+import express from "express";
+import { index } from "../../controller/setting.controller";
+
+export const userSettingRouter = express.Router();
+
+userSettingRouter.get("/", index);
