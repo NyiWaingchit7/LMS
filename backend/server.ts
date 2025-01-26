614dee3f900558da6c703ee19e4b4f9ca77ae7c9
NyiWaingchit7 <nyiwaingchit5@gmail.com>
2025-01-26 12:33:05 +0630

    fix

--- backend/server.ts
+++ backend/server.ts
@@ -1,35 +1,9 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { adminRouterGroup } from "@/routes/group/admin.rout";
+import { userRouterGroup } from "@/routes/group/user.route";
 dotenv.config();
-
-import multer from "multer";
-import { appRouter } from "@/routes/app.route";
-import { searchLecture } from "@/controller/lecture.controller";
-import { getPaymentBanks } from "@/controller/payment_bank.controller";
-import { createPurchase } from "@/controller/purchase.controller";
-import { categoryRouter } from "@/routes/category.route";
-import { homeRouter } from "@/routes/home.route";
-import { lectureRouter } from "@/routes/lecture.route";
-import { lessonRouter } from "@/routes/lesson.route";
-import { pageRouter } from "@/routes/page.route";
-import { paymentAccountRouter } from "@/routes/payment_account.route";
-import { paymentBankRouter } from "@/routes/payment_bank.route";
-import { pruchaseRouter } from "@/routes/purchase.route";
-import { settingRouter } from "@/routes/setting.route";
-import { studentRouter } from "@/routes/student.route";
-import { tagLineRouter } from "@/routes/tagline.route";
-import { userRouter } from "@/routes/user.route";
-import { userAuthRouter } from "@/routes/user/auth.route";
-import { userCategoryRouter } from "@/routes/user/category.route";
-import { userLectureRouter } from "@/routes/user/lecture.route";
-import { userLessonRouter } from "@/routes/user/lesson.route";
-import { userPaymentRouter } from "@/routes/user/payment.route";
-import { userSettingRouter } from "@/routes/user/setting.route";
-import { userTagLineRouter } from "@/routes/user/tagLine.route";
-import { checkauth } from "@/utils/auth";
-import { fileUpload, fileDelete } from "@/utils/fileUpload";
-import { popularLectureRouter } from "@/routes/popular_lecture.route";
 
 const app = express();
 
@@ -37,46 +11,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const upload = multer({ storage: multer.memoryStorage() });
-
 const port = 4000;
-
-app.use("/api/v1/admin/auth", appRouter);
-app.use("/api/v1/admin/users", userRouter);
-app.use("/api/v1/admin/categories", categoryRouter);
-app.use("/api/v1/admin/lectures", lectureRouter);
-app.use("/api/v1/admin/lessons", lessonRouter);
-app.use("/api/v1/admin/students", studentRouter);
-app.use("/api/v1/admin/payment-banks", paymentBankRouter);
-app.use("/api/v1/admin/payment-accounts", paymentAccountRouter);
-app.use("/api/v1/admin/purchases", pruchaseRouter);
-app.use("/api/v1/admin/pages", pageRouter);
-app.use("/api/v1/admin/home", homeRouter);
-app.use("/api/v1/admin/settings", settingRouter);
-app.use("/api/v1/admin/tag-lines", tagLineRouter);
-app.use("/api/v1/admin/popular-lectures", popularLectureRouter);
-
-// user
-app.use("/api/v1/categories", userCategoryRouter);
-app.use("/api/v1/lectures", userLectureRouter);
-app.use("/api/v1/lessons", userLessonRouter);
-app.use("/api/v1/payments", userPaymentRouter);
-app.use("/api/v1/tag-lines", userTagLineRouter);
-app.use("/api/v1/settings", userSettingRouter);
-app.use("/api/v1/popular-lectures", popularLectureRouter);
-
-app.use("/api/v1/auth", userAuthRouter);
-
-app.post(
-  "/api/v1/admin/file-upload",
-  checkauth,
-  upload.array("files"),
-  fileUpload
-);
-app.delete("/api/v1/admin/file-delete", checkauth, fileDelete);
-app.get("/api/v1/admin/get-lectures", checkauth, searchLecture);
-app.get("/api/v1/admin/get-paymentbanks", checkauth, getPaymentBanks);
-app.get("/api/v1/admin/create-purchase", checkauth, createPurchase);
+//admin
+app.use("/api/v1/admin", adminRouterGroup);
+//user
+app.use("api/v1", userRouterGroup);
+//default
 app.get("/", (req: Request, res: Response) => {
   res.send("hello");
 });
---
+++ backend/src/routes/group/admin.rout.ts
@@ -0,0 +1,46 @@
+import express, { Request, Response } from "express";
+import { searchLecture } from "@/controller/lecture.controller";
+import { getPaymentBanks } from "@/controller/payment_bank.controller";
+import { createPurchase } from "@/controller/purchase.controller";
+import { checkauth } from "@/utils/auth";
+import { fileUpload, fileDelete } from "@/utils/fileUpload";
+import { appRouter } from "../admin/app.route";
+import { categoryRouter } from "../admin/category.route";
+import { homeRouter } from "../admin/home.route";
+import { lectureRouter } from "../admin/lecture.route";
+import { lessonRouter } from "../admin/lesson.route";
+import { pageRouter } from "../admin/page.route";
+import { paymentAccountRouter } from "../admin/payment_account.route";
+import { paymentBankRouter } from "../admin/payment_bank.route";
+import { pruchaseRouter } from "../admin/purchase.route";
+import { settingRouter } from "../admin/setting.route";
+import { studentRouter } from "../admin/student.route";
+import { tagLineRouter } from "../admin/tagline.route";
+import { userRouter } from "../admin/user.route";
+import multer from "multer";
+
+export const adminRouterGroup = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
+adminRouterGroup.use("/auth", appRouter);
+adminRouterGroup.use("/users", userRouter);
+adminRouterGroup.use("/categories", categoryRouter);
+adminRouterGroup.use("/lectures", lectureRouter);
+adminRouterGroup.use("/lessons", lessonRouter);
+adminRouterGroup.use("/students", studentRouter);
+adminRouterGroup.use("/payment-banks", paymentBankRouter);
+adminRouterGroup.use("/payment-accounts", paymentAccountRouter);
+adminRouterGroup.use("/purchases", pruchaseRouter);
+adminRouterGroup.use("/pages", pageRouter);
+adminRouterGroup.use("/home", homeRouter);
+adminRouterGroup.use("/settings", settingRouter);
+adminRouterGroup.use("/tag-lines", tagLineRouter);
+adminRouterGroup.post(
+  "/file-upload",
+  checkauth,
+  upload.array("files"),
+  fileUpload
+);
+adminRouterGroup.delete("/file-delete", checkauth, fileDelete);
+adminRouterGroup.get("/get-lectures", checkauth, searchLecture);
+adminRouterGroup.get("/get-paymentbanks", checkauth, getPaymentBanks);
+adminRouterGroup.get("/create-purchase", checkauth, createPurchase);
