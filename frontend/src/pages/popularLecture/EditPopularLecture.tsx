6ee38dc5969d0cf2e223aaf3667e3fb34d40b2c2
NyiWaingchit7 <nyiwaingchit5@gmail.com>
2025-01-26 12:23:32 +0630

    popular-frontend

---
+++ frontend/src/pages/popularLecture/EditPopularLecture.tsx
@@ -0,0 +1,36 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { HeadLine } from "../../component/HeadLine";
+import { Layout } from "../../component/layout/Layout";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import {
+  handleShowPoppularLecture,
+  getlCreateLectures,
+  setPopularLectureData,
+} from "../../store/slice/popular_lectureSlice";
+import { PopularLecture } from "../../types/popular_lecture";
+import { Form } from "./partials/Form";
+
+export const EditPopuarLecture = () => {
+  const id = Number(useParams().id);
+  const dispatch = useAppDispatch();
+  const lecture = useAppSelector(
+    (store) => store.popularLecture.popularLecture
+  ) as PopularLecture;
+  const lectures = useAppSelector((store) => store.popularLecture.lectures);
+
+  useEffect(() => {
+    dispatch(handleShowPoppularLecture(id));
+    dispatch(getlCreateLectures());
+    return () => {
+      dispatch(setPopularLectureData(null));
+    };
+  }, [id]);
+  if (!lecture) return null;
+  return (
+    <Layout title="Edit Popuar Lecture">
+      <HeadLine header="Edit Popular Lecture" />
+      <Form lecture={lecture} lectures={lectures} />
+    </Layout>
+  );
+};
