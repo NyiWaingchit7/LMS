f311a5469ff4ba0fc002f17e5f15ffa44acf0c20
NyiWaingchit7 <nyiwaingchit5@gmail.com>
2025-01-30 10:26:01 +0630

    fix-server

--- backend/src/routes/user/popular_lecture.route.ts
+++ backend/src/routes/user/popular_lecture.route.ts
@@ -1,10 +1,8 @@
 import express from "express";
 
-import { checkauth } from "../../../utils/auth";
 import { index, show } from "../../controller/popular-lecture.controller";
 
 export const userPopularLectureRouter = express.Router();
-userPopularLectureRouter.use(checkauth);
 
 userPopularLectureRouter.get("/", index);
 
