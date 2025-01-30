f01e15afcbe22144063fa02c2b60be2ba89a6613
NyiWaingchit7 <nyiwaingchit5@gmail.com>
2025-01-30 09:54:45 +0630

    fix-path

--- backend/server.ts
+++ backend/server.ts
@@ -3,7 +3,7 @@ import "tsconfig-paths/register";
 
 import cors from "cors";
 import dotenv from "dotenv";
-import { adminRouterGroup } from "@/routes/group/admin.rout";
+import { adminRouterGroup } from "@/routes/group/admin.route";
 import { userRouterGroup } from "@/routes/group/user.route";
 dotenv.config();
 
