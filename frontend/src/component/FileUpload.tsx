9ee1e93ae536ee761d560cf6b0c9c473f03bc1fa
NyiWaingchit7 <nyiwaingchit5@gmail.com>
2025-01-31 11:49:32 +0630

    fix-api-token-admin

--- frontend/src/component/FileUpload.tsx
+++ frontend/src/component/FileUpload.tsx
@@ -7,7 +7,7 @@ import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
 import { useEffect, useState } from "react";
 import { config } from "../utils/config";
 import { Button } from "@mui/material";
-import { headerOptions } from "../utils/requestOption";
+import { generateToken, headerOptions } from "../utils/requestOption";
 import toast from "react-hot-toast";
 
 registerPlugin(FilePondPluginImagePreview);
@@ -22,6 +22,7 @@ export const FileUpload = ({ setImgUrl, editImg }: Props) => {
   const [edit, setEdit] = useState(false);
   const [fileRev, setFileRev] = useState("");
   const accessToken = localStorage.getItem("accessToken");
+  const [apitoken, setApiToken] = useState("");
 
   const server = {
     process: {
@@ -31,6 +32,7 @@ export const FileUpload = ({ setImgUrl, editImg }: Props) => {
       withCredentials: false,
       headers: {
         Authorization: `Bearer ${accessToken}`,
+        API_TOKEN: `Bearer ${apitoken}`,
       },
       onload: (data: any) => {
         const res = JSON.parse(data);
@@ -49,7 +51,7 @@ export const FileUpload = ({ setImgUrl, editImg }: Props) => {
     try {
       const response = await fetch(`${config.apiUrl}/file-delete`, {
         method: "DELETE",
-        headers: headerOptions(),
+        headers: await headerOptions(),
         body: JSON.stringify({ image: fileRev }),
       });
       const data = await response.json();
@@ -69,7 +71,13 @@ export const FileUpload = ({ setImgUrl, editImg }: Props) => {
       setEdit(true);
     }
   }, [editImg]);
-
+  useEffect(() => {
+    const getToken = async () => {
+      const data = await generateToken();
+      setApiToken(data);
+    };
+    getToken();
+  }, []);
   return (
     <div className="App">
       {!edit && (
